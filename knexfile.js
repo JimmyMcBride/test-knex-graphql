2c800cb9e15a1b059d6804ba8c28c2aca027db84
JimmyMcBride <mcbride967@gmail.com>
2020-05-03 23:58:58 -0500

    added rds

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,13 @@ require("dotenv").config();
 module.exports = {
   development: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: process.env.RDS_HOSTNAME,
+      user: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      port: process.env.RDS_PORT,
+      database: "postgres",
+    },
     migrations: {
       directory: "./src/data/migrations",
     },
